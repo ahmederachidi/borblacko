6babf423bb0a0f4e5f29ccf82961e1f952213e79
ahmederachidi <42737175+ahmederachidi@users.noreply.github.com>
2018-10-12 22:29:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
-client.on("ready", () => {
-let channel =     client.channels.get("499394830779285529","499628058018578432")
-setInterval(function() {
-channel.send(`**سبام يولد ام الفلة**`);
-}, 1000)
-})
-
-
 
  
 
@@ -23,29 +15,6 @@ client.on('message', async msg=> {
   }
 });
  
-var ServerID = "494162603275452427"; //اي دي السيرفر
-var ChannelID = "499394829717864459";// اي دي الروم
-
-
-client.on('warn', console.warn);
-
-client.on('error', console.error);
-
-
-client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Zeyad,Royale','\n','ProBot credits miner is ready!'));// Codes Server` - iiZeyad , Roýale"1.5k 🍷#1111
-
-client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
-
-client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// Codes server - !.Zeyad' , ♚Roýale#5555
-
-
-function timerFunc() {
-    client.on('message', msg => {
-        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
-
-
-    });
-}
 
 var timer = setTimeout(timerFunc, 1000);
 

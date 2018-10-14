2064b3d1bca7d5daf6a2de3d867fceff63cc3cad
ahmederachidi <42737175+ahmederachidi@users.noreply.github.com>
2018-10-14 21:31:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,16 @@
+
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on('ready', () => {
+    client.user.setActivity("ابيع كل شي")
+
+});
 
 client.on("ready", () => {
 let channel =     client.channels.get("500336272125067299")

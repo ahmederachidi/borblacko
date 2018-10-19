7d7bda10630d62e526f3a57649ec5e28703497de
ahmederachidi <42737175+ahmederachidi@users.noreply.github.com>
2018-10-19 17:04:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,6 +30,95 @@ function timerFunc() {
 
 var timer = setTimeout(timerFunc, 1000);
 
+
+
+  client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('!bc')){
+if(!message.author.id === '434641589680209932') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+
+
+
+client.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send('hjghjghj ')
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+
+client.on('message', message => {
+if (message.content === '!spam1') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send('hjghjghjghe ')
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+
+client.on('message', message => {
+if (message.content === '!spam2') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send('')
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+
+client.on('message', message => {
+if (message.content === '3') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send('afdgm ')
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+
+client.on('message', message => {
+if (message.content === '!spam4') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send('dfffgf ')
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+
+
+
 client.login(process.env.BOT_TOKE);
 
 

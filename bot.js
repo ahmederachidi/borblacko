eb54537b11ebb660c06f4010cd7ce467e5783039
ahmederachidi <42737175+ahmederachidi@users.noreply.github.com>
2018-10-20 16:54:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,32 @@
-
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const ahmed = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('╔[════════════════════════════════════]╗');
+  console.log('            ╔[════════════]╗')
+  console.log('              Bot Is Online')
+  console.log('            ╚[════════════]╝')
+  console.log('╚[════════════════════════════════════]╝')
+});
+
+console.log("Welcome Again !");
+ 
+
+
+client.on("message", message => {
+
+            if (message.content.startsWith("bc3")) {
+                         
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
 
 
 var ServerID = "500335405359693824"; //اي دي السيرفر
@@ -46,8 +71,49 @@ if (message.content === '!spam') {
 });
 
 
+var ServerID = "500335405359693824"; //اي دي السيرفر
+var ChannelID = "503218096815996938";// اي دي الروم
+
+
+ahmed.on('warn', console.warn);
+
+ahmed.on('error', console.error);
+
+
+ahmed.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Zeyad,Royale','\n','ProBot credits miner is ready!'));// Codes Server` - iiZeyad , Roýale"1.5k 🍷#1111
+
+ahmed.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+ahmed.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// Codes server - !.Zeyad' , ♚Roýale#5555
+
+
+function timerFunc() {
+    ahmed.on('message', msg => {
+        ahmed.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+
+    });
+}
+
+var timer = setTimeout(timerFunc, 1000);
+
+
+    ahmed.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) 
+        message.channel.send(Math.random().toString(36).substring(7))
+          .then(m => {
+            count++;
+          })
+
+        
+      }
+});
+
 client.login(process.env.BOT_TOKEN);
 
-client.login(process.env.BOT_TOKE);
+ahmed.login(process.env.BOT_TOKE);
 
 

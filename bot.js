3f8f0c22a9cc2e6eafc7920eee3c21c930aa535d
ahmederachidi <42737175+ahmederachidi@users.noreply.github.com>
2018-12-10 13:15:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,13 @@ const s3 = new Discord.Client();
 const s2 = new Discord.Client();
 const s1 = new Discord.Client();
 const s8 = new Discord.Client();
+const s15 = new Discord.Client();
+const s14 = new Discord.Client();
+const s13 = new Discord.Client();
+const s12 = new Discord.Client();
+const s11 = new Discord.Client();
+const s10 = new Discord.Client();
+const s9 = new Discord.Client();
 
 var prefix = "!"
 
@@ -351,6 +358,299 @@ s8.on('message', msg => {
 
 
 
+s9.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+  let args = message.content.split(" ").slice(1);
+
+
+
+
+  if (command === "s9") {
+
+          message.delete()
+
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+
+  }
+
+
+
+});
+
+s10.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+  let args = message.content.split(" ").slice(1);
+
+
+
+
+  if (command === "s10") {
+
+          message.delete()
+
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+
+  }
+
+
+
+});
+
+s11.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command === "s11") {
+
+          message.delete()
+
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+
+  }
+
+
+
+});
+
+s12.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+  let args = message.content.split(" ").slice(1);
+
+
+
+
+  if (command === "s12") {
+
+          message.delete()
+
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+
+  }
+
+
+
+});
+
+
+s9.on("ready", () => {
+let channel =     s9.channels.get("521651016668676101")
+setInterval(function() {
+channel.send(Math.random().toString(36).substring(7))
+}, 50)
+})
+
+s10.on("ready", () => {
+let channel =     s10.channels.get("520709744827039764")
+setInterval(function() {
+channel.send(Math.random().toString(36).substring(7))
+}, 50)
+})
+
+s11.on("ready", () => {
+let channel =     s11.channels.get("521651041524121602")
+setInterval(function() {
+channel.send(Math.random().toString(36).substring(7))
+}, 50)
+})
+
+s12.on("ready", () => {
+let channel =     s12.channels.get("521655191435477012")
+setInterval(function() {
+channel.send(Math.random().toString(36).substring(7))
+}, 50)
+})
+
+s13.on("ready", () => {
+let channel =     s13.channels.get("521655209110405131")
+setInterval(function() {
+channel.send(Math.random().toString(36).substring(7))
+}, 50)
+})
+
+s14.on("ready", () => {
+let channel =     s14.channels.get("521655223693869056")
+setInterval(function() {
+channel.send(Math.random().toString(36).substring(7))
+}, 50)
+})
+
+s15.on("ready", () => {
+let channel =     s15.channels.get("521655236578770944")
+setInterval(function() {
+channel.send(Math.random().toString(36).substring(7))
+}, 50)
+})
+
+
+
+s13.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+  let args = message.content.split(" ").slice(1);
+
+
+// -s
+
+  if (command === "s13") {
+
+          message.delete()
+
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+
+  }
+
+
+
+});
+s14.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+  let args = message.content.split(" ").slice(1);
+
+
+// -s
+
+  if (command === "s14") {
+
+          message.delete()
+
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+
+  }
+
+
+
+});
+s15.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+  let args = message.content.split(" ").slice(1);
+
+
+// -s
+
+  if (command === "s15") {
+
+          message.delete()
+
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+
+  }
+
+
+
+});
+
+
+s9.on('message', msg => {
+  if (msg.content === 'فلوس') {
+    msg.channel.send('#credits');
+  }
+});
+
+s10.on('message', msg => {
+  if (msg.content === 'فلوس') {
+    msg.channel.send('#credits');
+  }
+});
+s11.on('message', msg => {
+  if (msg.content === 'فلوس') {
+    msg.channel.send('#credits');
+  }
+});
+s12.on('message', msg => {
+  if (msg.content === 'فلوس') {
+    msg.channel.send('#credits');
+  }
+});
+s13.on('message', msg => {
+  if (msg.content === 'فلوس') {
+    msg.channel.send('#credits');
+  }
+});
+s14.on('message', msg => {
+  if (msg.content === 'فلوس') {
+    msg.channel.send('#credits');
+  }
+});
+s15.on('message', msg => {
+  if (msg.content === 'فلوس') {
+    msg.channel.send('#credits');
+  }
+});
+
+
+
+
+
+
+
 
 
 
@@ -362,4 +662,12 @@ s5.login(process.env.BOT_5);
 s6.login(process.env.BOT_6);
 s7.login(process.env.BOT_7);
 s8.login(process.env.BOT_8);
+s9.login(process.env.BOT_9);
+s10.login(process.env.BOT_10);
+s11.login(process.env.BOT_11);
+s12.login(process.env.BOT_12);
+s13.login(process.env.BOT_13);
+s14.login(process.env.BOT_14);
+s15.login(process.env.BOT_15);
+
 
